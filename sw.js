9b26ea500e9d8ce2c9e94b30f8a6260ab4a13868
Yasir Afaq <32522237+yasir2afaq@users.noreply.github.com>
2026-09-09 10:37:30 +0530

    v12

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='facultyflow-v12';
+const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon-180.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match('./index.html'))))});
