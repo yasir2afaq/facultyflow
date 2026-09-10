a12f893ba74c8ef5ede67d4f1620673fb7668a02
Yasir Afaq <32522237+yasir2afaq@users.noreply.github.com>
2026-09-10 12:33:47 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='facultyflow-v34';
-const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon-180.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r}).catch(()=>caches.match('./index.html'))))});
